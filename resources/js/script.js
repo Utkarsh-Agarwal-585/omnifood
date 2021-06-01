f5dca75fcb36b25b0336cf6bb2757d06fef850bd
Utkarsh Agarwal <agrawalu01@gmail.com>
2021-06-01 16:57:26 +0530

    sticky navbar,slideToggle issue fixed

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,4 +1,16 @@
 $(document).ready(function() {
+
+  $('.js--section-features').waypoint(function(direction) {
+    if (direction == "down") {
+        $('nav').addClass('sticky');
+    } else {
+        $('nav').removeClass('sticky');
+    }
+}, {
+  offset: '60px;'
+});
+
+
    $('.js--scroll-plans').click(function () {
        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top},1000);
    });
@@ -51,7 +63,16 @@ $(document).ready(function() {
 
   $('.js--nav').click(function() {
     var nav = $('.js--main');
+    var icon = $('.js--nav i');
     nav.slideToggle(200);
+    if(icon.hasClass('ion-navicon-round')){
+      icon.addClass('ion-close-round');
+      icon.removeClass('ion-navicon-round');
+    }
+    else{
+      icon.addClass('ion-navicon-round');
+      icon.removeClass('ion-close-round');
+    }
      
 });
 });
